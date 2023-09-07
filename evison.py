28b9eec92bdfcff71a08b13cdc2b8ee1669fe4e8
lewiskimaru <139235175+lewiskimaru@users.noreply.github.com>
2023-09-07 18:24:10 +0300

    Update evison.py

--- evison.py
+++ evison.py
@@ -1,7 +1,10 @@
+from __future__ import print_function
+
 import streamlit as st
 import tempfile
 from roboflow import Roboflow
 from PIL import Image
+import africastalking
 
 rf = Roboflow(api_key="aQONY7aSjUN7H1sSqu0s")
 project = rf.workspace().project("e-waste-detection-model")
@@ -9,6 +12,44 @@ model = project.version(3).model
 
 # Page configuration
 st.set_page_config(page_title="e vision", page_icon="🚮")
+
+#########################################################
+user_phone = st.text_input("Enter Your Phone Number: ")
+
+# Display the entered text
+if user_input:
+    st.write(f"You entered: {user_phone}")
+    
+class AIRTIME:
+    def __init__(self):
+		# Set your app credentials
+        self.username = "mild.ke"
+        self.api_key = "daae7b33a138c168292fbe863cd135d3b33b5f768639cef6f70d6ba141e0d8b1"
+
+        # Initialize the SDK
+        africastalking.initialize(self.username, self.api_key)
+
+        # Get the airtime service
+        self.airtime = africastalking.Airtime
+
+    def send(self):
+        # Set phone_number in international format
+        phone_number = user_phone
+
+        # Set The 3-Letter ISO currency code and the amount
+        amount ="05.00"
+        currency_code = "KES"
+
+        try:
+                # That's it hit send and we'll take care of the rest
+                responses = self.airtime.send(phone_number=phone_number, amount=amount, currency_code=currency_code)
+                print (responses)
+        except Exception as e:
+                print ("Encountered an error while sending airtime:%s" %str(e))
+
+####################################
+# Reward User
+tokens = 0
 
 st.title("e-vision")
 st.markdown("Detect E-WASTE using AI")
@@ -24,7 +65,11 @@ if uploaded_file is not None:
     try:
         st.spinner("Processing.......")
         prediction = model.predict(temp_path, confidence=40, overlap=30)
-        st.success("Results")
+        tokens += 2
+        st.success("Tokens = tokens")
+        if __name__ == '__main__':
+            AIRTIME().send()
+        
     except:
         st.info("Failed, Upload another image")
             
